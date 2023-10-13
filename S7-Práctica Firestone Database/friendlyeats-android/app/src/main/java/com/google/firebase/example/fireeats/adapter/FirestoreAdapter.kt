72cbc6b9ccda2de1ab8259bdfa85de6b5f821d2b
xELDIEGOx <juandiegoat508@gmail.com>
2023-10-12 21:36:45 -0600

    Trabajo semana 7

---
+++ S7-Práctica Firestone Database/friendlyeats-android/app/src/main/java/com/google/firebase/example/fireeats/adapter/FirestoreAdapter.kt
@@ -0,0 +1,129 @@
+package com.google.firebase.example.fireeats.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import android.util.Log
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import java.util.ArrayList
+
+
+/**
+ * RecyclerView adapter for displaying the results of a Firestore [Query].
+ *
+ * Note that this class forgoes some efficiency to gain simplicity. For example, the result of
+ * [DocumentSnapshot.toObject] is not cached so the same object may be deserialized
+ * many times as the user scrolls.
+ */
+abstract class FirestoreAdapter<VH : RecyclerView.ViewHolder>(private var query: Query?) :
+    RecyclerView.Adapter<VH>(),
+    EventListener<QuerySnapshot> {
+
+    override fun onEvent(documentSnapshots: QuerySnapshot?, e: FirebaseFirestoreException?) {
+
+        // Handle errors
+        if (e != null) {
+            Log.w(TAG, "onEvent:error", e)
+            return
+        }
+
+        // Dispatch the event
+        if (documentSnapshots != null) {
+            for (change in documentSnapshots.documentChanges) {
+                // snapshot of the changed document
+                when (change.type) {
+                    DocumentChange.Type.ADDED -> {
+                        onDocumentAdded(change) // Add this line
+                    }
+                    DocumentChange.Type.MODIFIED -> {
+                        onDocumentModified(change) // Add this line
+                    }
+                    DocumentChange.Type.REMOVED -> {
+                        onDocumentRemoved(change) // Add this line
+                    }
+                }
+            }
+        }
+
+        onDataChanged()
+    }
+
+private var registration: ListenerRegistration? = null
+
+private val snapshots = ArrayList<DocumentSnapshot>()
+
+    private fun onDocumentAdded(change: DocumentChange) {
+        snapshots.add(change.newIndex, change.document)
+        notifyItemInserted(change.newIndex)
+    }
+
+    private fun onDocumentModified(change: DocumentChange) {
+        if (change.oldIndex == change.newIndex) {
+            // Item changed but remained in same position
+            snapshots[change.oldIndex] = change.document
+            notifyItemChanged(change.oldIndex)
+        } else {
+            // Item changed and changed position
+            snapshots.removeAt(change.oldIndex)
+            snapshots.add(change.newIndex, change.document)
+            notifyItemMoved(change.oldIndex, change.newIndex)
+        }
+    }
+
+    private fun onDocumentRemoved(change: DocumentChange) {
+        snapshots.removeAt(change.oldIndex)
+        notifyItemRemoved(change.oldIndex)
+    }
+
+    fun startListening() {
+        if (registration == null) {
+            registration = query?.addSnapshotListener(this)
+        }
+    }
+
+    fun stopListening() {
+        registration?.remove()
+        registration = null
+
+        snapshots.clear()
+        notifyDataSetChanged()
+    }
+
+    fun setQuery(query: Query) {
+        // Stop listening
+        stopListening()
+
+        // Clear existing data
+        snapshots.clear()
+        notifyDataSetChanged()
+
+        // Listen to new query
+        this.query = query
+        startListening()
+    }
+
+
+
+    open fun onError(e: FirebaseFirestoreException) {
+        Log.w(TAG, "onError", e)
+    }
+
+    open fun onDataChanged() {}
+
+    override fun getItemCount(): Int {
+        return snapshots.size
+    }
+
+    protected fun getSnapshot(index: Int): DocumentSnapshot {
+        return snapshots[index]
+    }
+
+    companion object {
+
+        private const val TAG = "FirestoreAdapter"
+    }
+}
---
+++ S7-Práctica Firestone Database/friendlyeats-android/build.gradle.kts
@@ -0,0 +1,43 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("androidx.navigation.safeargs") version "2.7.4" apply false
+    id("com.github.ben-manes.versions") version "0.49.0" apply true
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+fun isNonStable(candidate: ModuleComponentIdentifier): Boolean {
+    return listOf("alpha", "beta", "rc", "snapshot").any { keyword ->
+        keyword in candidate.version.lowercase()
+    }
+}
+
+fun isBlockListed(candidate: ModuleComponentIdentifier): Boolean {
+    return listOf("androidx.browser:browser").any { keyword ->
+        keyword in candidate.toString().lowercase()
+    }
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate) || isBlockListed(candidate)
+    }
+}
+
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+}
